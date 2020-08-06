34e3afc0909c589f2cf6f2103796274d77ff9e5b
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-07 01:59:24 +0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
+  console.log(`Server is running on port ${config.PORT}`);
 });
